9d0115917b0ed8845934b66efcf9a6145d7178d8
ishan-26 <125141868+ishan-26@users.noreply.github.com>
2023-05-04 17:23:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const cards = document.querySelectorAll(".card");
+
+timeTag =
+document.querySelector(".time b"),
+
+flipsTag = document.querySelector(".flips b"),
+
+refreshBtn = document.querySelector(".details button");
+
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matched = 0;
+let cardOne,cardTwo;
+let disableDeck = false;
+
+
+function initTimer() {
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+}
+
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCard() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        
+     card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
+refreshBtn.addEventListener("click", shuffleCard);
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
